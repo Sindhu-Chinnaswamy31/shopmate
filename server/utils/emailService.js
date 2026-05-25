4059102fcc55c4258d7a8f46d5f5190182fa327c
Sindhu C <sindhuc@LTP0373.local>
2026-05-25 15:05:35 +0530

    feat: order status email notifications

--- server/utils/emailService.js
+++ server/utils/emailService.js
@@ -2,9 +2,10 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
+  family: 4, // Use IPv4
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
+    pass: process.env.EMAIL_PASS,
   }
 });
 
